5c9f67aaefe5db036529581c1b9ed25c7ae0fa24
Jeyaseelan R <jeyaseelan.work@gmail.com>
2024-02-17 23:52:58 +0530

    Modify front end url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatterbox-frontend.vercel.app",
         methods: ["GET", "POST"]
     }
 });
